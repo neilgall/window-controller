3eed079cc1ac97e6a5916b2b4d37a109c718836c
Neil Gall <neilgall@mac.com>
2019-08-26 22:01:36 +0100

    Reduce logging

--- device/iot_listener.py
+++ device/iot_listener.py
@@ -13,7 +13,7 @@ thingName = "fresh-air"
 clientId = "fresh-air-buttons"
 
 logger = logging.getLogger("AWSIoTPythonSDK.core")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 streamHandler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 streamHandler.setFormatter(formatter)
